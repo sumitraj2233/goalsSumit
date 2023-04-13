6263cc53a8b0454b0feffdf6db1bd65a48e811dd
sumitraj2233 <sumitraj983541@gmail.com>
2023-04-13 16:19:41 +0530

    mongodb setup done

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const colors = require("colors");
+const { connectDB } = require("./config/db");
+const { errorHandler } = require("./middleware/errorMiddleWare");
 const app = express();
 const port = process.env.PORT_NUMBER || 3000;
+connectDB();
 app.listen(port, () => console.log(`Server running on port${port}`));
 // app.get("/api/goals", (req, res) => {
 //   res.status(200).json({
 //     message: "goals fetched",
 //   });
 // });
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use("/api/goals", require("./routes/goalRoutes"));
+app.use(errorHandler);
